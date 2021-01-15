89c58d2b3d3856c5b8c6a8cab0dca687f26d504f
Nazca <iarnazca@gmail.com>
2021-01-15 13:09:20 -0800

    typo

--- src/classes/GameManager.php
+++ src/classes/GameManager.php
@@ -10,7 +10,7 @@
 ///
 
 /*
-This is the model is should represent game data operations and rulesets.  It manages the behavior and data of the application,
+This is the model it should represent game data operations and rulesets.  It manages the behavior and data of the application,
 responds to requests about it's application data and responds to instructions to change state.  This file governs all game data
 and the rules applied to a users ability to update that data. This file will monitor the state of it's own data and govern the transformation
 of all of that data.
@@ -20,8 +20,8 @@ This class will access the SQL database with a seperate Data Access Object proba
 */
 class GameManager {
   //properties
-  public $user ; //the database id of the currently logged in user. 0 or null if no user is logged in.
-  public $username; //String username to show what user is logged in;
+  public $userid ; //the database id of the currently logged in user. 0, null, or unset if no user is logged in.
+
 
 
   //get_class_methods
