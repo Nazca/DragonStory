d8efe6b80d1203c82f02923303fcf779b2c23e63
Nazca <iarnazca@gmail.com>
2021-01-15 10:40:50 -0800

    Moved footer on display pages to footer.php for ease of editing

--- src/patchNotes.php
+++ src/patchNotes.php
@@ -71,67 +71,7 @@
 
 					</div>
 
-					<!-- Footer -->
-						<footer id="footer">
-							<section>
-								<h2>Inspired by my Wife.</h2>
-								<p>I strait up asked my wife for a good idea and she strait up gave me on.  Dragon Story to me is a fun game that is Easy to learn but hard to master.  A child can get an egg, feed it and raise it, play with it, do activities with it and generally have fun with a cool dragon.  For the adult crowd it's a dragon breeding game.  The dragons all have genetics and breeding will take some time.  I love you wife and thank you for the great idea.</p>
-								<ul class="actions">
-									<li><a href="generic.html" class="button">Learn More</a></li>
-								</ul>
-							</section>
-							<section>
-								<h2>Dragon Story</h2>
-								<dl class="alt">
-									<dt>Address</dt>
-									<dd>Rancho Cucamonga, CA 91730 &bull; USA</dd>
-									<dt>Phone</dt>
-									<dd>(432) 208-4053</dd>
-									<dt>Email</dt>
-									<dd>newell.john.k@gmail.com</dd>
-								</dl>
-								<ul class="icons">
-									<li><a href="#" class="icon brands fa-twitter alt"><span class="label">Twitter</span></a></li>
-									<li><a href="#" class="icon brands fa-facebook-f alt"><span class="label">Facebook</span></a></li>
-									<li><a href="#" class="icon brands fa-instagram alt"><span class="label">Instagram</span></a></li>
-									<li><a href="#" class="icon brands fa-github alt"><span class="label">GitHub</span></a></li>
-									<li><a href="#" class="icon brands fa-dribbble alt"><span class="label">Dribbble</span></a></li>
-								</ul>
-							</section>
-							<p class="copyright">&copy; John Newell's Dragon Story. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
-						</footer>
-
-				</div>
-
-			<!-- Scripts -->
-				<script src="assets/js/jquery.min.js"></script>
-				<script src="assets/js/jquery.scrollex.min.js"></script>
-				<script src="assets/js/jquery.scrolly.min.js"></script>
-				<script src="assets/js/browser.min.js"></script>
-				<script src="assets/js/breakpoints.min.js"></script>
-				<script src="assets/js/util.js"></script>
-				<script src="assets/js/main.js"></script>
-
-
-
-
-
-
-
-
-		<!-- END my site or application content -->
-
-
-
-		<script src="js/vendor/modernizr-{{MODERNIZR_VERSION}}.min.js"></script>
-		<script src="js/app.js"></script>
-
-		<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-		<script>
-		window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-		ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
-		</script>
-		<script src="https://www.google-analytics.com/analytics.js" async></script>
-		</body>
-
-		</html>
+				<?php
+				//require the footer so that the page will be complete.
+					require('footer.php');
+				 ?>
