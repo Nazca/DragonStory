b1f0dd5c6c0826446c5eb7548f9aa501e4f08a40
Nazca <iarnazca@gmail.com>
2021-01-15 12:34:47 -0800

    engine.php comments

--- src/engine.php
+++ src/engine.php
@@ -1,3 +1,10 @@
 <?php
 //this is the main file called when the user is viewing, giving input, or otherwise monitering the gamestate.
+
+//it's possible that we've ended up here without sending any log in information.
+
+//we should check to see if a user is logged in real quick  If they are and we have no input, display the game state on the home page
+//if we have log in information lets give that to the InputController so that we may clean it, modify the game state, and display it to the users
+
+//if they are not logged in and we have no input lets redirect to index.php
  ?>
