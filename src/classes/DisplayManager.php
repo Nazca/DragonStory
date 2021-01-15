af4a2d7b3142f0beebe1d01aff4727a2ea2da8e5
Nazca <iarnazca@gmail.com>
2021-01-14 18:22:55 -0800

    DisplayManager Class update

--- src/classes/DisplayManager.php
+++ src/classes/DisplayManager.php
@@ -5,6 +5,10 @@
 ///The Display Manager accepts commands from the GameManager to set variables and game display options.
 ///</summary>
 ///
+
+/*
+This Display Manager governs the visualization of the data thatis passed to it by the Model class GameManager.
+*/
 class DisplayManager {
   //properties
 
