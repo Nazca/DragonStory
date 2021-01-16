4d50bb4c2b773605914d53ee9fb9301e9a315020
Nazca <iarnazca@gmail.com>
2021-01-16 12:41:48 -0800

    even with mysqlnd enabled on the server get_result() still wont work.

--- src/includes/login.inc.php
+++ src/includes/login.inc.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require('functions.inc.php');
+require('classes/iimysqli_result.php');
 
 if (($_SESSION['userid'] = checkUserLogin($_POST['email'], $_POST['password'])) && (checkUserLogin($_POST['email'], $_POST['password']) !== false)){
   //we are logged in, go to the engine
