1a7503a84cd4187a125afe564dba0e15461f0eed
Nazca <iarnazca@gmail.com>
2021-01-16 10:38:39 -0800

    comment

--- src/includes/config.inc.php
+++ src/includes/config.inc.php
@@ -1,6 +1,8 @@
 <?php
 //config file for application settings such as sql server connection information
 
+//NOTE: We will eventually need to change these settings based on whether or not the url is localhost or not.
+
 $SQLserver = "localhost" ;
 $SQLuser = "root" ;
 $SQLpassword = "nhojjohn#";
