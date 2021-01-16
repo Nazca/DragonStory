d42fd65dd84c24a67b6f6ba7fcd59f5c53e05d43
Nazca <iarnazca@gmail.com>
2021-01-16 15:36:09 -0800

    updated login error handler

--- src/includes/login.inc.php
+++ src/includes/login.inc.php
@@ -2,6 +2,11 @@
 session_start();
 require('functions.inc.php');
 
+if (!isset($_POST['email'] || !isset($_POST['password']))){
+  //user didn't fill out the fields
+  header('location: ../login.php');
+  exit();
+}
 
 if (($_SESSION['userid'] = checkUserLogin($_POST['email'], $_POST['password'])) && (checkUserLogin($_POST['email'], $_POST['password']) !== false)){
   //we are logged in, go to the engine
