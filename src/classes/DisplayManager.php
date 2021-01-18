f871b167b100ef9442f4dc554322876a45bbe902
Nazca <iarnazca@gmail.com>
2021-01-18 11:07:15 -0800

    bugfix.  only the number turns blue now

--- src/classes/DisplayManager.php
+++ src/classes/DisplayManager.php
@@ -18,11 +18,23 @@ class DisplayManager {
   public function displayHome(){
     $userManager = new UserManager();
 
+
+    if (isset($_GET['number'])){
+      $number = $_GET['number'] ;
+
+    }else{
+      $number = 0 ;
+
+    }
+
+
+
     if ($userManager->userLoggedIn() === true){
       //we are logged in.
       echo "We are logged in. UID:"  . $_SESSION['userid'];
       echo "<br /><a href=\"engine.php?logout=true\">Logout</a>";
-      echo "<br /><a href=\"engine.php\">Engine</a>";
+      echo "<br /><a href=\"engine.php?number=" . $number + 1 . "\">Engine</a>";
+
       ?>
 
       <!--this is placeholder html for gamestate view -->
@@ -31,6 +43,8 @@ class DisplayManager {
           <center>
             <br /><br /><br />
             <h3>Dragon Story</h3>
+
+            <p><?php if ($number % 100 == 0 && $number > 9){ echo "<font color=blue>";}else{echo "<font>";} ; if (isset($number) && $number > 9){ echo $number ;}?></font></p>
             <p>
               This page is still under construction.  Don't worry we have your email and will email you when things heat up a little bit.<br />
               You can press the blue log out button to log out and the blue engine button to simulate an engine action.
